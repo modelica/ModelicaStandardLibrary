modelica/ModelicaStandardLibrary

commit 21f046bbb486284d25689ad5ea3c8f54121961a8

#935 fixed (Modelica.Blocks.Math.RootMeanSquare might fail):
Added a parameter in block "Mean" to state, if the output is guaranteed to be >= 0.0.
In such a case, the output of block "Mean" is computed as max(0, y_computed), i.e., it is
guaranteed to be >= 0.0. In the RootMeanSquare block, this parameter is appropriately set and
then it is guaranteed that the input to the Sqrt block is >= 0.0.

Additionally, added a test model in ModelicaTest to check the desired behavior.



git-svn-id: https://svn.modelica.org/projects/Modelica/trunk@5860 7ce873d0-865f-4ce7-a662-4bb36ea78beb

Modelica/Blocks/Math.mo
@@ -3173,6 +3173,9 @@ This block calculates the components <code>y_re</code> and <code>y_im</code> of 
     extends Modelica.Blocks.Interfaces.SISO;
     parameter Modelica.SIunits.Frequency f(start=50) "Base frequency";
     parameter Real x0=0 "Start value of integrator state";
+    parameter Boolean yGreaterOrEqualZero=false
+        "=true, if output y is guaranteed to be >= 0 for the exact solution"
+        annotation (Evaluate=true, Dialog(tab="Advanced"));
   protected
     parameter Modelica.SIunits.Time t0(fixed=false) "Start time of simulation";
     Real x(start=x0, fixed=true) "Integrator state";
@@ -3181,7 +3184,7 @@ This block calculates the components <code>y_re</code> and <code>y_im</code> of 
   equation
     der(x) = u;
     when sample(t0+1/f, 1/f) then
-      y=f*x;
+      y = if not yGreaterOrEqualZero then f*x else max(0.0, f*x);
       reinit(x, 0);
     end when;
     annotation (Documentation(info="<html>
@@ -3195,6 +3198,14 @@ T 0
 </pre>
 <p>
 Note: The output is updated after each period defined by 1/f.
+</p>
+
+<p>
+If parameter <b>yGreaterOrEqualZero</b> in the Advanced tab is <b>true</b> (default = <b>false</b>),
+then the modeller provides the information that the mean of the input signal is guaranteed
+to be &ge; 0 for the exact solutihn. However, due to inaccuracies in the numerical integration scheme,
+the output might be slightly negative. If this parameter is set to true, then the output is
+explicitly set to 0.0, if the mean value results in a negative value.
 </p>
 </html>"),   Icon(graphics={Text(
             extent={{-80,60},{80,20}},
@@ -3249,7 +3260,7 @@ Note: The output is updated after each period defined by 1/f.
     parameter Modelica.SIunits.Frequency f(start=50) "Base frequency";
     MultiProduct        product(nu=2)
       annotation (Placement(transformation(extent={{-40,-10},{-20,10}})));
-    Mean mean(final f=f)
+    Mean mean(final f=f, final yGreaterOrEqualZero=true)
       annotation (Placement(transformation(extent={{0,-10},{20,10}})));
     Blocks.Math.Sqrt sqrt1
       annotation (Placement(transformation(extent={{40,-10},{60,10}})));

ModelicaTest/Blocks.mo
@@ -443,6 +443,31 @@ extends Modelica.Icons.ExamplesPackage;
     annotation (experiment(StopTime=3));
   end IntegerTable;
 
+model RootMeanSquare
+  extends Modelica.Icons.Example;
+  Modelica.Blocks.Math.Mean mean1(f=expSine.freqHz)
+    annotation (Placement(transformation(extent={{-40,40},{-20,60}})));
+  Modelica.Blocks.Sources.ExpSine expSine(
+    freqHz=2,
+    damping=0.5,
+    offset=-0.5)
+    annotation (Placement(transformation(extent={{-80,40},{-60,60}})));
+  Modelica.Blocks.Math.RootMeanSquare rootMeanSquare(f=expSine.freqHz)
+    annotation (Placement(transformation(extent={{-40,0},{-20,20}})));
+equation
+  connect(expSine.y, mean1.u) annotation (Line(
+      points={{-59,50},{-42,50}},
+      color={0,0,127},
+      smooth=Smooth.None));
+  connect(expSine.y, rootMeanSquare.u) annotation (Line(
+      points={{-59,50},{-52,50},{-52,10},{-42,10}},
+      color={0,0,127},
+      smooth=Smooth.None));
+  annotation (experiment(StopTime=2), Diagram(coordinateSystem(
+          preserveAspectRatio=false, extent={{-100,-100},{100,100}}), graphics));
+end RootMeanSquare;
+
+
   package FilterTests "Test of Blocks.Continuous.Filter"
     extends Modelica.Icons.ExamplesPackage;
     model AllOptions

commit 13c87b10b493cbb02611eb39c4e8cfa6697cd5b9

Fixes a small typo (introduced in r5860)
git-svn-id: https://svn.modelica.org/projects/Modelica/trunk@5861 7ce873d0-865f-4ce7-a662-4bb36ea78beb

Modelica/Blocks/Math.mo
@@ -3203,7 +3203,7 @@ Note: The output is updated after each period defined by 1/f.
 <p>
 If parameter <b>yGreaterOrEqualZero</b> in the Advanced tab is <b>true</b> (default = <b>false</b>),
 then the modeller provides the information that the mean of the input signal is guaranteed
-to be &ge; 0 for the exact solutihn. However, due to inaccuracies in the numerical integration scheme,
+to be &ge; 0 for the exact solution. However, due to inaccuracies in the numerical integration scheme,
 the output might be slightly negative. If this parameter is set to true, then the output is
 explicitly set to 0.0, if the mean value results in a negative value.
 </p>