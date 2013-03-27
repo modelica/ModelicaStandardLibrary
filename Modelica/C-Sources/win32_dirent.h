3e71aeed749b58dfc244892908780f293aa22bd4
sjoelund.se <sjoelund.se@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2013-03-27 15:38:20 +0000

    Removing Thumbs.db from 2.2.2 sources...
    git-svn-id: https://svn.modelica.org/projects/Modelica/branches/maintenance/2.2.2@6146 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- Modelica/C-Sources/win32_dirent.h
+++ Modelica/C-Sources/win32_dirent.h
@@ -32,12 +32,12 @@ extern "C" {
 
 struct dirent
 {
-	long		d_ino;		        /* Always zero. */
-	unsigned short	d_reclen;	  /* Always zero. */
-	unsigned short	d_namlen;	  /* Length of name in d_name. */
-	char*		d_name;		        /* File name. */
-	/* NOTE: The name in the dirent structure points to the name in the
-	 *       finddata_t structure in the DIR. */
+  long    d_ino;            /* Always zero. */
+  unsigned short  d_reclen;    /* Always zero. */
+  unsigned short  d_namlen;    /* Length of name in d_name. */
+  char*    d_name;            /* File name. */
+  /* NOTE: The name in the dirent structure points to the name in the
+   *       finddata_t structure in the DIR. */
 };
 
 /*
@@ -46,40 +46,40 @@ struct dirent
  */
 typedef struct
 {
-	/* disk transfer area for this dir */
-	struct _finddata_t	dd_dta;
+  /* disk transfer area for this dir */
+  struct _finddata_t  dd_dta;
 
-	/* dirent struct to return from dir (NOTE: this makes this thread
-	 * safe as long as only one thread uses a particular DIR struct at
-	 * a time) */
-	struct dirent		dd_dir;
+  /* dirent struct to return from dir (NOTE: this makes this thread
+   * safe as long as only one thread uses a particular DIR struct at
+   * a time) */
+  struct dirent    dd_dir;
 
-	/* _findnext handle */
-	long			dd_handle;
+  /* _findnext handle */
+  long      dd_handle;
 
-	/*
+  /*
          * Status of search:
-	 *   0 = not started yet (next entry to read is first entry)
-	 *  -1 = off the end
-	 *   positive = 0 based index of next entry
-	 */
-	short			dd_stat;
+   *   0 = not started yet (next entry to read is first entry)
+   *  -1 = off the end
+   *   positive = 0 based index of next entry
+   */
+  short      dd_stat;
 
-	/* given path for dir with search pattern (struct is extended) */
-	char			dd_name[1];
+  /* given path for dir with search pattern (struct is extended) */
+  char      dd_name[1];
 } DIR;
 
 
-DIR*		opendir (const char*);
-struct dirent*	readdir (DIR*);
-int		closedir (DIR*);
-void		rewinddir (DIR*);
-long		telldir (DIR*);
-void		seekdir (DIR*, long);
+DIR*    opendir (const char*);
+struct dirent*  readdir (DIR*);
+int    closedir (DIR*);
+void    rewinddir (DIR*);
+long    telldir (DIR*);
+void    seekdir (DIR*, long);
 
-#ifdef	__cplusplus
+#ifdef  __cplusplus
 }
 #endif
 
-#endif	/* Not _DIRENT_H_ */
+#endif  /* Not _DIRENT_H_ */
 
