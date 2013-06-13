a78ddd1b30c6380ef1464f369391839638d6960b
beutlich <beutlich@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2013-06-13 09:23:53 +0000

    refs #1151 (on ModelicaStrings.c):  * added the {{{MODELICA_EXPORT}}} define  * renamed jump label {{{error}}} to {{{Modelica_ERROR}}} (as previously was done in ModelicaInternal.c)  * replaced {{{sscanf}}} by calls to {{{strtod}}}/{{{strtol}}}. As a side effect: There now is no longer a dependency on {{{NO_FILE_SYSTEM}}} as {{{strtod}}}/{{{strtol}}} are defined in stdlib.h (whereas {{{sscanf}}} is in stdio.h). That means that on target platforms where ''Modelica.Utilities.Strings.scanReal'' and ''Modelica.Utilities.Strings.scanInteger'' previously failed due to a missing file system (e.g. dSPACE, xPC) it is now also supported.  * avoid locale dependency of string to number conversion
    git-svn-id: https://svn.modelica.org/projects/Modelica/trunk@6670 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- Modelica/Resources/C-Sources/ModelicaStrings.c
+++ Modelica/Resources/C-Sources/ModelicaStrings.c
@@ -1,6 +1,19 @@
 /* External utility functions for Modelica package
    Modelica.Functions.Strings.
 
+   The functions are mostly non-portable. The following #define's are used
+   to define the system calls of the operating system
+
+    _MSC_VER       : Microsoft Visual C++
+    MODELICA_EXPORT: Prefix used for function calls. If not defined, blank is used
+                     Useful definitions:
+                     - "static" that is all functions become static
+                       (useful if file is included with other C-sources for an
+                        embedded system)
+                     - "__declspec(dllexport)" if included in a DLL and the
+                       functions shall be visible outside of the DLL
+
+
    Release Notes:
       Mar. 26, 2013: by Martin Otter, DLR.
                      Introduced three (int) casts to avoid warning messages (ticket #1032).
@@ -24,23 +37,27 @@
       Jan.  7, 2002: by Martin Otter, DLR.
                      Implemented a first version
 
-   Copyright (C) 2002-2006, Modelica Association and DLR.
+   Copyright (C) 2002-2013, Modelica Association and DLR.
 
    The content of this file is free software; it can be redistributed
    and/or modified under the terms of the Modelica License 2, see the
    license conditions and the accompanying disclaimer in file
    Modelica/ModelicaLicense2.html or in Modelica.UsersGuide.ModelicaLicense2.
 */
-
-#include <ctype.h>
-#if !defined(NO_FILE_SYSTEM)
-#include <stdio.h>
+#if !defined(MODELICA_EXPORT)
+#   define MODELICA_EXPORT
 #endif
-#include <string.h>
+
 #include "ModelicaUtilities.h"
+#if defined(linux)
+#define _GNU_SOURCE 1
+#endif
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
 
-
-const char* ModelicaStrings_substring(const char* string, int startIndex, int endIndex) {
+MODELICA_EXPORT const char* ModelicaStrings_substring(const char* string, int startIndex, int endIndex) {
 
   /* Return string1(startIndex:endIndex) if endIndex >= startIndex,
      or return string1(startIndex:startIndex), if endIndex = 0.
@@ -77,14 +94,14 @@ const char* ModelicaStrings_substring(const char* string, int startIndex, int en
 }
 
 
-int ModelicaStrings_length(const char* string)
+MODELICA_EXPORT int ModelicaStrings_length(const char* string)
 /* Returns the number of characters "string" */
 {
      return (int) strlen(string);
 }
 
 
-int ModelicaStrings_compare(const char* string1, const char* string2, int caseSensitive)
+MODELICA_EXPORT int ModelicaStrings_compare(const char* string1, const char* string2, int caseSensitive)
 /* compares two strings, optionally ignoring case */
 {
     int result;
@@ -113,7 +130,7 @@ int ModelicaStrings_compare(const char* string1, const char* string2, int caseSe
 
 #define MAX_TOKEN_SIZE 100
 
-int ModelicaStrings_skipWhiteSpace(const char* string, int i)
+MODELICA_EXPORT int ModelicaStrings_skipWhiteSpace(const char* string, int i)
 /* Return index in string after skipping ws, or position of terminating nul. */
 {
     while (string[i-1] != '\0' && isspace(string[i-1]))
@@ -184,7 +201,7 @@ static int MatchUnsignedInteger(const char* string, int start)
 /* --------------- end of utility functions used in scanXXX functions ----------- */
 
 
-void ModelicaStrings_scanIdentifier(const char* string, int startIndex, int* nextIndex, const char** identifier)
+MODELICA_EXPORT void ModelicaStrings_scanIdentifier(const char* string, int startIndex, int* nextIndex, const char** identifier)
 {
     int token_length=0;
 
@@ -218,7 +235,7 @@ void ModelicaStrings_scanIdentifier(const char* string, int startIndex, int* nex
     return;
 }
 
-void ModelicaStrings_scanInteger(const char* string, int startIndex, int unsignedNumber,
+MODELICA_EXPORT void ModelicaStrings_scanInteger(const char* string, int startIndex, int unsignedNumber,
                                  int* nextIndex, int* integerNumber)
 {
     int number_length=0;
@@ -236,28 +253,43 @@ void ModelicaStrings_scanInteger(const char* string, int startIndex, int unsigne
         /* Number of characters in unsigned number. */
 
         if (number_length > 0 && sign + number_length < MAX_TOKEN_SIZE) {
-          /* check if the scanned string is no Real number */
-          int next = token_start + sign + number_length - 1;
-          if (  string[next] == '\0' ||
-               (string[next] != '\0' && string[next] != '.'
-                                     && string[next] != 'e'
-                                     && string[next] != 'E') ) {
-             /* get Integer value */
-             char buf[MAX_TOKEN_SIZE+1];
-             int x;
-
-             strncpy(buf, string+token_start-1, sign + number_length);
-             buf[sign + number_length] = '\0';
-#if !defined(NO_FILE_SYSTEM)
-             if (sscanf(buf, "%d", &x) == 1) {
-                *integerNumber = x;
-                *nextIndex = token_start + sign + number_length;
-                return;
-             }
+            /* check if the scanned string is no Real number */
+            int next = token_start + sign + number_length - 1;
+            if (  string[next] == '\0' ||
+                 (string[next] != '\0' && string[next] != '.'
+                                       && string[next] != 'e'
+                                       && string[next] != 'E') ) {
+#if defined(_MSC_VER) && _MSC_VER >= 1400
+                _locale_t loc = _create_locale(LC_NUMERIC, "C");
+#elif defined(linux)
+                locale_t loc = newlocale(LC_NUMERIC, "C", NULL);
 #endif
-          } else {
-            ++number_length;
-          }
+                char buf[MAX_TOKEN_SIZE+1];
+                /* Buffer for copying the part recognized as the number for passing to strtol(). */
+                char* endptr;
+                /* For error checking of strtol(). */
+                int x;
+                /* For receiving the result. */
+
+                strncpy(buf, string+token_start-1, sign + number_length);
+                buf[sign + number_length] = '\0';
+#if defined(_MSC_VER) && _MSC_VER >= 1400
+                x = (int)_strtol_l(buf, &endptr, 10, loc);
+                _free_locale(loc);
+#elif defined(linux)
+                x = (int)strtol_l(buf, &endptr, 10, loc);
+                freelocale(loc);
+#else
+                x = (int)strtol(buf, &endptr, 10);
+#endif
+                if (*endptr == 0) {
+                    *integerNumber = x;
+                    *nextIndex = token_start + sign + number_length;
+                    return;
+                }
+            } else {
+                ++number_length;
+            }
         }
     }
 
@@ -267,7 +299,7 @@ void ModelicaStrings_scanInteger(const char* string, int startIndex, int unsigne
      return;
 }
 
-void ModelicaStrings_scanReal(const char* string, int startIndex, int unsignedNumber,
+MODELICA_EXPORT void ModelicaStrings_scanReal(const char* string, int startIndex, int unsignedNumber,
                               int* nextIndex, double* number)
 {
     /*
@@ -294,17 +326,11 @@ void ModelicaStrings_scanReal(const char* string, int startIndex, int unsignedNu
     /* Total number of characters recognized as part of the non-numeric parts
      * of exponent (the 'e' and the sign). */
 
-    char buf[MAX_TOKEN_SIZE+1];
-    /* Buffer for copying the part recognized as the number for passing to sscanf(). */
-
-    double x;
-    /* For receiving the result. */
-
     /* Scan sign of decimal number */
 
     if (string[token_start-1] == '+' || string[token_start-1] == '-') {
         total_length = 1;
-        if (unsignedNumber==1) goto error;
+        if (unsignedNumber==1) goto Modelica_ERROR;
     }
 
     /* Scan integer part of mantissa. */
@@ -332,34 +358,65 @@ void ModelicaStrings_scanReal(const char* string, int startIndex, int unsignedNu
             exp_len += 1;
         }
         len = MatchUnsignedInteger(string, token_start + total_length + exp_len);
-        if (len == 0) goto error;
+        if (len == 0) goto Modelica_ERROR;
         total_length += exp_len + len;
     }
 
     /* Convert accumulated characters into a number. */
 
     if (total_length > 0 && total_length < MAX_TOKEN_SIZE) {
+#if defined(_MSC_VER) && _MSC_VER >= 1400
+        _locale_t loc = _create_locale(LC_NUMERIC, "C");
+#elif defined(linux)
+        locale_t loc = newlocale(LC_NUMERIC, "C", NULL);
+#else
+        char* dec = localeconv()->decimal_point;
+#endif
+        char buf[MAX_TOKEN_SIZE+1];
+        /* Buffer for copying the part recognized as the number for passing to strtod(). */
+        char* endptr;
+        /* For error checking of strtod(). */
+        double x;
+        /* For receiving the result. */
+
         strncpy(buf, string+token_start-1, total_length);
         buf[total_length] = '\0';
-#if !defined(NO_FILE_SYSTEM)
-        if (sscanf(buf, "%lg", &x) == 1) {
+#if defined(_MSC_VER) && _MSC_VER >= 1400
+        x = _strtod_l(buf, &endptr, loc);
+        _free_locale(loc);
+#elif defined(linux)
+        x = strtod_l(buf, &endptr, loc);
+        freelocale(loc);
+#else
+        if (*dec == '.') {
+            x = strtod(buf, &endptr);
+        }
+        else if (NULL == strchr(buf, '.')) {
+            x = strtod(buf, &endptr);
+        }
+        else {
+            char* p = strchr(buf, '.');
+            *p = *dec;
+            x = strtod(buf, &endptr);
+        }
+#endif
+        if (*endptr == 0) {
             *number = x;
             *nextIndex = token_start + total_length;
             return;
         }
-#endif
     }
 
     /* Token missing or cannot be converted to result type. */
 
-error:
+Modelica_ERROR:
     *nextIndex = startIndex;
     *number = 0;
     return;
 }
 
 
-void ModelicaStrings_scanString(const char* string, int startIndex,
+MODELICA_EXPORT void ModelicaStrings_scanString(const char* string, int startIndex,
                                 int* nextIndex, const char** result)
 {
     int i, token_start, past_token, token_length;
@@ -367,12 +424,12 @@ void ModelicaStrings_scanString(const char* string, int startIndex,
     token_length = 0;
     token_start = ModelicaStrings_skipWhiteSpace(string, startIndex);
     i = token_start;
-    if (string[token_start-1] != '"') goto error;
+    if (string[token_start-1] != '"') goto Modelica_ERROR;
     /* Index of first char of token, after ws. */
 
     ++i;
     while (1) {
-        if (string[i-1] == '\0') goto error;
+        if (string[i-1] == '\0') goto Modelica_ERROR;
         if (string[i-2] == '\\' && string[i-1] == '"')
             ; /* escaped quote, consume */
         else if (string[i-1] == '"')
@@ -393,7 +450,7 @@ void ModelicaStrings_scanString(const char* string, int startIndex,
         return;
     }
 
-error:
+Modelica_ERROR:
     *result = ModelicaAllocateString(0);
     *nextIndex = startIndex;
     return;
