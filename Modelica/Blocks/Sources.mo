modelica/ModelicaStandardLibrary

commit 1c9ad202d12eed99e95da4088319a9c983bcd0a2

#1175 fix improve (Unspecified format of tables text files): Added an updated documentation
git-svn-id: https://svn.modelica.org/projects/Modelica/trunk@6730 7ce873d0-865f-4ce7-a662-4bb36ea78beb

Modelica/Blocks/Sources.mo
@@ -2547,15 +2547,16 @@ double tab2(6,2)   # another comment line
 Note, that the first two characters in the file need to be
 \"#1\" (a line comment defining the version number of the file format).
 Afterwards, the corresponding matrix has to be declared
-with type, name and actual dimensions. Finally, in successive
-rows of the file, the elements of the matrix have to be given.
-A matrix row can span several lines (so the newline character
-is allowed in a matrix row). Numbers have to be given according
-to C syntax (such as 2.3, -2, +2.e4). Number separators are spaces,
-tab (\t), comma (,), or semicolon (;).
+with type (= "double" or "float"), name and actual dimensions.
+Finally, in successive rows of the file, the elements of the matrix
+have to be given. The elements have to be provided as a sequence of
+numbers in row-wise order (therefore a matrix row can span several
+lines in the file and need not start at the beginning of a line).
+Numbers have to be given according to C syntax (such as 2.3, -2, +2.e4).
+Number separators are spaces, tab (\t), comma (,), or semicolon (;).
 Several matrices may be defined one after another. Line comments start
 with the hash symbol (#) and can appear everywhere.
-Other text, like trailing non comments, is not allowed.
+Other characters, like trailing non comments, are not allowed in the file.
 </p>
 <p>
 MATLAB is a registered trademark of The MathWorks, Inc.

Modelica/Blocks/Tables.mo
@@ -212,15 +212,16 @@ double tab2(5,2)   # another comment line
 Note, that the first two characters in the file need to be
 \"#1\" (a line comment defining the version number of the file format).
 Afterwards, the corresponding matrix has to be declared
-with type, name and actual dimensions. Finally, in successive
-rows of the file, the elements of the matrix have to be given.
-A matrix row can span several lines (so the newline character
-is allowed in a matrix row). Numbers have to be given according
-to C syntax (such as 2.3, -2, +2.e4). Number separators are spaces,
-tab (\t), comma (,), or semicolon (;).
+with type (= "double" or "float"), name and actual dimensions.
+Finally, in successive rows of the file, the elements of the matrix
+have to be given. The elements have to be provided as a sequence of
+numbers in row-wise order (therefore a matrix row can span several
+lines in the file and need not start at the beginning of a line).
+Numbers have to be given according to C syntax (such as 2.3, -2, +2.e4).
+Number separators are spaces, tab (\t), comma (,), or semicolon (;).
 Several matrices may be defined one after another. Line comments start
 with the hash symbol (#) and can appear everywhere.
-Other text, like trailing non comments, is not allowed.
+Other characters, like trailing non comments, are not allowed in the file.
 </p>
 <p>
 MATLAB is a registered trademark of The MathWorks, Inc.
@@ -511,15 +512,16 @@ double tab2(5,2)   # another comment line
 Note, that the first two characters in the file need to be
 \"#1\" (a line comment defining the version number of the file format).
 Afterwards, the corresponding matrix has to be declared
-with type, name and actual dimensions. Finally, in successive
-rows of the file, the elements of the matrix have to be given.
-A matrix row can span several lines (so the newline character
-is allowed in a matrix row). Numbers have to be given according
-to C syntax (such as 2.3, -2, +2.e4). Number separators are spaces,
-tab (\t), comma (,), or semicolon (;).
+with type (= "double" or "float"), name and actual dimensions.
+Finally, in successive rows of the file, the elements of the matrix
+have to be given. The elements have to be provided as a sequence of
+numbers in row-wise order (therefore a matrix row can span several
+lines in the file and need not start at the beginning of a line).
+Numbers have to be given according to C syntax (such as 2.3, -2, +2.e4).
+Number separators are spaces, tab (\t), comma (,), or semicolon (;).
 Several matrices may be defined one after another. Line comments start
 with the hash symbol (#) and can appear everywhere.
-Other text, like trailing non comments, is not allowed.
+Other characters, like trailing non comments, are not allowed in the file.
 </p>
 <p>
 MATLAB is a registered trademark of The MathWorks, Inc.
@@ -806,15 +808,16 @@ double table2D_2(4,4)   # comment line
 Note, that the first two characters in the file need to be
 \"#1\" (a line comment defining the version number of the file format).
 Afterwards, the corresponding matrix has to be declared
-with type, name and actual dimensions. Finally, in successive
-rows of the file, the elements of the matrix have to be given.
-A matrix row can span several lines (so the newline character
-is allowed in a matrix row). Numbers have to be given according
-to C syntax (such as 2.3, -2, +2.e4). Number separators are spaces,
-tab (\t), comma (,), or semicolon (;).
+with type (= "double" or "float"), name and actual dimensions.
+Finally, in successive rows of the file, the elements of the matrix
+have to be given. The elements have to be provided as a sequence of
+numbers in row-wise order (therefore a matrix row can span several
+lines in the file and need not start at the beginning of a line).
+Numbers have to be given according to C syntax (such as 2.3, -2, +2.e4).
+Number separators are spaces, tab (\t), comma (,), or semicolon (;).
 Several matrices may be defined one after another. Line comments start
 with the hash symbol (#) and can appear everywhere.
-Other text, like trailing non comments, is not allowed.
+Other characters, like trailing non comments, are not allowed in the file.
 The matrix elements are interpreted in exactly the same way
 as if the matrix is given as a parameter. For example, the first
 column \"table2D_1[2:,1]\" contains the u[1] grid points,

commit dbcfca0fa369fde02218236ba027578804e01ea0

refs #1175: fixed syntax errors introduced in r6730
git-svn-id: https://svn.modelica.org/projects/Modelica/trunk@6735 7ce873d0-865f-4ce7-a662-4bb36ea78beb

Modelica/Blocks/Sources.mo
@@ -2547,7 +2547,7 @@ double tab2(6,2)   # another comment line
 Note, that the first two characters in the file need to be
 \"#1\" (a line comment defining the version number of the file format).
 Afterwards, the corresponding matrix has to be declared
-with type (= "double" or "float"), name and actual dimensions.
+with type (= \"double\" or \"float\"), name and actual dimensions.
 Finally, in successive rows of the file, the elements of the matrix
 have to be given. The elements have to be provided as a sequence of
 numbers in row-wise order (therefore a matrix row can span several

Modelica/Blocks/Tables.mo
@@ -212,7 +212,7 @@ double tab2(5,2)   # another comment line
 Note, that the first two characters in the file need to be
 \"#1\" (a line comment defining the version number of the file format).
 Afterwards, the corresponding matrix has to be declared
-with type (= "double" or "float"), name and actual dimensions.
+with type (= \"double\" or \"float\"), name and actual dimensions.
 Finally, in successive rows of the file, the elements of the matrix
 have to be given. The elements have to be provided as a sequence of
 numbers in row-wise order (therefore a matrix row can span several
@@ -512,7 +512,7 @@ double tab2(5,2)   # another comment line
 Note, that the first two characters in the file need to be
 \"#1\" (a line comment defining the version number of the file format).
 Afterwards, the corresponding matrix has to be declared
-with type (= "double" or "float"), name and actual dimensions.
+with type (= \"double\" or \"float\"), name and actual dimensions.
 Finally, in successive rows of the file, the elements of the matrix
 have to be given. The elements have to be provided as a sequence of
 numbers in row-wise order (therefore a matrix row can span several
@@ -808,7 +808,7 @@ double table2D_2(4,4)   # comment line
 Note, that the first two characters in the file need to be
 \"#1\" (a line comment defining the version number of the file format).
 Afterwards, the corresponding matrix has to be declared
-with type (= "double" or "float"), name and actual dimensions.
+with type (= \"double\" or \"float\"), name and actual dimensions.
 Finally, in successive rows of the file, the elements of the matrix
 have to be given. The elements have to be provided as a sequence of
 numbers in row-wise order (therefore a matrix row can span several