3fa1b5261c3a4f7f08b94a8c1cc4fd6b23eada09
beutlich <beutlich@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2013-10-17 09:50:34 +0000

    merged r7083 from trunk refs #1273: use a safe check for the locale-dependent versions of strtod and strtol on GNU/Linux
    git-svn-id: https://svn.modelica.org/projects/Modelica/branches/maintenance/3.2.1@7084 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- Modelica/Resources/C-Sources/ModelicaStrings.c
+++ Modelica/Resources/C-Sources/ModelicaStrings.c
@@ -47,7 +47,7 @@
 #if !defined(MODELICA_EXPORT)
 #   define MODELICA_EXPORT
 #endif
-#if defined(__linux__) && !defined(_DS1006)
+#if defined(__gnu_linux__)
 #define _GNU_SOURCE 1
 #endif
 
@@ -261,7 +261,7 @@ MODELICA_EXPORT void ModelicaStrings_scanInteger(const char* string, int startIn
                                        && string[next] != 'E') ) {
 #if defined(_MSC_VER) && _MSC_VER >= 1400
                 _locale_t loc = _create_locale(LC_NUMERIC, "C");
-#elif defined(__linux__) && !defined(_DS1006)
+#elif defined(__GNU_LIBRARY__) && __GNU_LIBRARY__ >= 6 && __GLIBC_PREREQ(2, 3)
                 locale_t loc = newlocale(LC_NUMERIC, "C", NULL);
 #endif
                 char buf[MAX_TOKEN_SIZE+1];
@@ -276,7 +276,7 @@ MODELICA_EXPORT void ModelicaStrings_scanInteger(const char* string, int startIn
 #if defined(_MSC_VER) && _MSC_VER >= 1400
                 x = (int)_strtol_l(buf, &endptr, 10, loc);
                 _free_locale(loc);
-#elif defined(__linux__) && !defined(_DS1006)
+#elif defined(__GNU_LIBRARY__) && __GNU_LIBRARY__ >= 6 && __GLIBC_PREREQ(2, 3)
                 x = (int)strtol_l(buf, &endptr, 10, loc);
                 freelocale(loc);
 #else
@@ -367,7 +367,7 @@ MODELICA_EXPORT void ModelicaStrings_scanReal(const char* string, int startIndex
     if (total_length > 0 && total_length < MAX_TOKEN_SIZE) {
 #if defined(_MSC_VER) && _MSC_VER >= 1400
         _locale_t loc = _create_locale(LC_NUMERIC, "C");
-#elif defined(__linux__) && !defined(_DS1006)
+#elif defined(__GNU_LIBRARY__) && __GNU_LIBRARY__ >= 6 && __GLIBC_PREREQ(2, 3)
         locale_t loc = newlocale(LC_NUMERIC, "C", NULL);
 #else
         char* dec = localeconv()->decimal_point;
@@ -384,7 +384,7 @@ MODELICA_EXPORT void ModelicaStrings_scanReal(const char* string, int startIndex
 #if defined(_MSC_VER) && _MSC_VER >= 1400
         x = _strtod_l(buf, &endptr, loc);
         _free_locale(loc);
-#elif defined(__linux__) && !defined(_DS1006)
+#elif defined(__GNU_LIBRARY__) && __GNU_LIBRARY__ >= 6 && __GLIBC_PREREQ(2, 3)
         x = strtod_l(buf, &endptr, loc);
         freelocale(loc);
 #else
