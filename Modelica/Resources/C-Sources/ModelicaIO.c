1199f6e842d2fca55e0028807e6ea4b93ca54c2d
otter <otter@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2016-01-14 07:48:11 +0000

    ModelicaIO.c improved: In case a new file shall be created with ModelicaIO_writeRealMatrix and this is not possible (e.g. if version 7.3 is not supported), then the error message was "Not possible ot open file", which is wrong. Changed the error message in this case to "Not possible to newly create file xxx (maybe version 7.3 not supported)
    git-svn-id: https://svn.modelica.org/projects/Modelica/trunk@8915 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- Modelica/Resources/C-Sources/ModelicaIO.c
+++ Modelica/Resources/C-Sources/ModelicaIO.c
@@ -320,13 +320,18 @@ MODELICA_EXPORT int ModelicaIO_writeRealMatrix(const char* fileName,
         matc = MAT_COMPRESSION_NONE;
     }
 
-    mat = append == 0 ?
-        Mat_CreateVer(fileName, NULL, matv) :
-        Mat_Open(fileName, (int)MAT_ACC_RDWR | matv);
-
-    if (mat == NULL) {
-        ModelicaFormatError("Not possible to open file \"%s\"\n", fileName);
-        return 0;
+    if ( append == 0 ) {
+        mat = Mat_CreateVer(fileName, NULL, matv);
+        if (mat == NULL) {
+           ModelicaFormatError("Not possible to newly create file \"%s\"\n(maybe version 7.3 not supported)\n", fileName);
+           return 0;
+        }        
+    } else {
+        mat = Mat_Open(fileName, (int)MAT_ACC_RDWR | matv);
+        if (mat == NULL) {
+           ModelicaFormatError("Not possible to open file \"%s\"\n", fileName);         
+           return 0;
+        }
     }
 
     /* MAT file array is stored column-wise -> need to transpose */
