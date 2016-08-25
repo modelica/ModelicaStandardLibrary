modelica/ModelicaStandardLibrary

commit 283c4d819bae7d757323872a959ae973b5343461

Added rotor displacement angle according to #2050
git-svn-id: https://svn.modelica.org/projects/Modelica/trunk@9417 7ce873d0-865f-4ce7-a662-4bb36ea78beb

Modelica/Blocks/Math.mo
@@ -2513,6 +2513,84 @@ zero or negative.
 </html>"));
   end Log10;
 
+  block WrapAngle "Wrap angle to interval [-pi,pi]"
+
+    extends Interfaces.SISO;
+  equation
+    y = Modelica.Math.wrapAngle(u);
+    annotation (
+      Icon(coordinateSystem(preserveAspectRatio=true, extent={{-100,-100},{100,
+              100}}), graphics={
+          Line(points={{-80,54},{-80,54},{-60,80},{-60,-80},{60,80},{60,-80},{
+                80,-52}},
+              color={0,0,0}),
+          Polygon(
+            points={{90,0},{68,8},{68,-8},{90,0}},
+            lineColor={192,192,192},
+            fillColor={192,192,192},
+            fillPattern=FillPattern.Solid),
+          Line(points={{-90,0},{68,0}},     color={192,192,192}),
+          Line(points={{0,-80},{0,68}}, color={192,192,192}),
+          Polygon(
+            points={{0,90},{-8,68},{8,68},{0,90}},
+            lineColor={192,192,192},
+            fillColor={192,192,192},
+            fillPattern=FillPattern.Solid)}),
+      Diagram(coordinateSystem(preserveAspectRatio=true, extent={{-100,-100},{
+              100,100}}), graphics={Line(points={{-80,80},{-88,80}}, color={192,
+            192,192}),Line(points={{-80,-80},{-88,-80}}, color={192,192,192}),
+            Line(points={{-80,-90},{-80,84}}, color={192,192,192}),Text(
+              extent={{-65,96},{-38,78}},
+              lineColor={160,160,164},
+              textString="y"),Polygon(
+              points={{-80,100},{-86,84},{-74,84},{-80,100}},
+              lineColor={192,192,192},
+              fillColor={192,192,192},
+              fillPattern=FillPattern.Solid),Line(points={{-100,0},{84,0}},
+            color={192,192,192}),Polygon(
+              points={{100,0},{84,6},{84,-6},{100,0}},
+              lineColor={192,192,192},
+              fillColor={192,192,192},
+              fillPattern=FillPattern.Solid),Line(points={{-79.8,-80},{-79.2,-50.6},
+            {-78.4,-37},{-77.6,-28},{-76.8,-21.3},{-75.2,-11.4},{-72.8,-1.31},{
+            -69.5,8.08},{-64.7,17.9},{-57.5,28},{-47,38.1},{-31.8,48.1},{-10.1,
+            58},{22.1,68},{68.7,78.1},{80,80}}),Text(
+              extent={{70,-3},{90,-23}},
+              textString="20",
+              lineColor={0,0,255}),Text(
+              extent={{-78,-1},{-58,-21}},
+              textString="1",
+              lineColor={0,0,255}),Text(
+              extent={{-109,72},{-89,88}},
+              textString=" 1.3",
+              lineColor={0,0,255}),Text(
+              extent={{-109,-88},{-89,-72}},
+              textString="-1.3",
+              lineColor={0,0,255}),Text(
+              extent={{62,30},{90,10}},
+              lineColor={160,160,164},
+              textString="u")}),
+      Documentation(info="<html>
+<p>
+This blocks computes the output <b>y</b> as the
+<i>base 10 logarithm</i> of the input <b>u</b>:
+</p>
+<pre>
+    y = <b>log10</b>( u );
+</pre>
+<p>
+An error occurs if the elements of the input <b>u</b> are
+zero or negative.
+</p>
+
+<p>
+<img src=\"modelica://Modelica/Resources/Images/Math/log10.png\"
+     alt=\"log10.png\">
+</p>
+
+</html>"));
+  end WrapAngle;
+
   block RealToInteger "Convert Real to Integer signal"
     extends Modelica.Blocks.Icons.IntegerBlock;
   public
@@ -3515,6 +3593,8 @@ connected with continuous blocks or with sampled-data blocks.
 </p>
 </html>", revisions="<html>
 <ul>
+<li><i>August 24, 2016</i>
+       by Christian Kral: added WrapAngle</li>
 <li><i>October 21, 2002</i>
        by <a href=\"http://www.robotic.dlr.de/Martin.Otter/\">Martin Otter</a>
        and Christian Schweiger:<br>

Modelica/Math/package.mo
@@ -13275,6 +13275,8 @@ Copyright &copy; 1998-2016, Modelica Association and DLR.
 </p>
 </html>", revisions="<html>
 <ul>
+<li><i>August 24, 2016</i>
+       by Christian Kral: added wrapAngle</li>
 <li><i>October 21, 2002</i>
        by <a href=\"http://www.robotic.dlr.de/Martin.Otter/\">Martin Otter</a>
        and Christian Schweiger:<br>

Modelica/Math/package.order
@@ -25,6 +25,7 @@ acosh
 exp
 log
 log10
+wrapAngle
 baseIcon1
 baseIcon2
 tempInterpol1

Modelica/Math/wrapAngle.mo
@@ -1,0 +1,23 @@
+within Modelica.Math;
+function wrapAngle "Wrap angle to interval ]-pi,pi]"
+  extends Modelica.Math.Icons.AxisCenter;
+  import Modelica.Constants.pi;
+  input SI.Angle u;
+  output SI.Angle y;
+
+algorithm
+  y :=mod(u, 2*pi);
+  if y>pi then
+    y := y - 2*pi;
+  end if;
+
+  annotation (Icon(graphics={
+        Line(points={{-90,0},{68,0}},     color={192,192,192}),
+        Polygon(
+          points={{90,0},{68,8},{68,-8},{90,0}},
+          lineColor={192,192,192},
+          fillColor={192,192,192},
+          fillPattern=FillPattern.Solid),
+        Line(points={{-80,54},{-80,54},{-60,80},{-60,-80},{60,80},{60,-80},{80,-52}},
+            color={0,0,0})}));
+end wrapAngle;

commit 0001699d1f27ab251437da3ad6328560a97f9132

Fixed wrong documentation introduced in r9417 and #2050
git-svn-id: https://svn.modelica.org/projects/Modelica/trunk@9419 7ce873d0-865f-4ce7-a662-4bb36ea78beb

Modelica/Blocks/Math.mo
@@ -2513,7 +2513,7 @@ zero or negative.
 </html>"));
   end Log10;
 
-  block WrapAngle "Wrap angle to interval [-pi,pi]"
+  block WrapAngle "Wrap angle to interval ]-pi,pi]"
 
     extends Interfaces.SISO;
   equation
@@ -2572,20 +2572,7 @@ zero or negative.
               textString="u")}),
       Documentation(info="<html>
 <p>
-This blocks computes the output <b>y</b> as the
-<i>base 10 logarithm</i> of the input <b>u</b>:
-</p>
-<pre>
-    y = <b>log10</b>( u );
-</pre>
-<p>
-An error occurs if the elements of the input <b>u</b> are
-zero or negative.
-</p>
-
-<p>
-<img src=\"modelica://Modelica/Resources/Images/Math/log10.png\"
-     alt=\"log10.png\">
+This blocks wraps the input angle into the interval ]-pi,pi].
 </p>
 
 </html>"));