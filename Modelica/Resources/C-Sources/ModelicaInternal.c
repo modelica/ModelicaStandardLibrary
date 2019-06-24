a2e253064305c979537ad8f1aa2b25ae0faf6609
Thomas Beutlich <modelica@tbeu.de>
2019-06-24 11:27:52 +0200

    Fix comment

--- Modelica/Resources/C-Sources/ModelicaInternal.c
+++ Modelica/Resources/C-Sources/ModelicaInternal.c
@@ -212,7 +212,7 @@ void ModelicaInternal_setenv(_In_z_ const char* name,
 
   #if defined(__MINGW32__) || defined(__CYGWIN__) /* MinGW and Cygwin have dirent.h */
     #include <dirent.h>
-  #else /* include the opendir/readdir/closedir implementation for _WIN32 */
+  #else /* include the opendir/readdir/closedir interface for _WIN32 */
     #include "win32_dirent.h"
   #endif
 
