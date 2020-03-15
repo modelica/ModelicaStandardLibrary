8626c6475c055fa5df8328c55ff8fb7bb791b1e8
Thomas Beutlich <modelica@tbeu.de>
2020-03-15 21:32:04 +0100

    Add unit tests

--- .CI/Test/ModelicaStrings.c
+++ .CI/Test/ModelicaStrings.c
@@ -7,5 +7,12 @@ const char* ModelicaStrings_substring(const char* string,
                                       int startIndex, int endIndex);
 
 int main(int argc, char **argv) {
-    assert(0 == strcmp("bcd", ModelicaStrings_substring("abcdef",2,4)));
+    assert(0 == strcmp("a", ModelicaStrings_substring("abc",-1,-1)));
+    assert(0 == strcmp("b", ModelicaStrings_substring("abc",2,-1)));
+    assert(0 == strcmp("", ModelicaStrings_substring("abc",2,0)));
+    assert(0 == strcmp("", ModelicaStrings_substring("abc",2,1)));
+    assert(0 == strcmp("b", ModelicaStrings_substring("abc",2,2)));
+    assert(0 == strcmp("bc", ModelicaStrings_substring("abc",2,3)));
+    assert(0 == strcmp("bc", ModelicaStrings_substring("abc",2,4)));
+    assert(0 == strcmp("", ModelicaStrings_substring("abc",4,4)));
 }
