bb6a43d0f8f96a234ee129494751bb565fced919
Thomas Beutlich <modelica@tbeu.de>
2020-03-30 10:30:13 +0200

    Add copyright and comment header

--- .CI/classCoverage/CountClasses.py
+++ .CI/classCoverage/CountClasses.py
@@ -1,11 +1,21 @@
-allClasses = dict([(c.strip(),0) for c in open("all.classes","r").readlines() if c.strip()])
-for line in open("models.uses", "r").readlines():
-  line = line.strip().strip('"')
-  if "" == line:
-    continue
-  if line not in allClasses.keys():
-    continue
-  allClasses[line] += 1
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-for cl in sorted(allClasses.keys(), key = lambda cl: (allClasses[cl],cl)):
-  print("%d %s" % (allClasses[cl],cl))
+'''
+Copyright (C) 2020, Modelica Association and contributors
+All rights reserved.
+
+Print class coverage for all classes, sorted by reference count
+'''
+
+allClasses = dict([(c.strip(), 0) for c in open("all.classes", "r").readlines() if c.strip()])
+for line in open("models.uses", "r").readlines():
+    line = line.strip().strip('"')
+    if "" == line:
+      continue
+    if line not in allClasses.keys():
+      continue
+    allClasses[line] += 1
+
+for cl in sorted(allClasses.keys(), key = lambda cl: (allClasses[cl], cl)):
+    print("{0} {1}".format(allClasses[cl], cl))
