f06c39ae9c5fa1418b2d34fe46697407c6a27af5
Thomas Beutlich <modelica@tbeu.de>
2020-10-12 17:14:44 +0200

    Fix and extend table tests (#3643)

--- .CI/Test/TablesNoUsertab.c
+++ .CI/Test/TablesNoUsertab.c
@@ -2,12 +2,22 @@
 #include "Common.c"
 
 #include <assert.h>
+#include <math.h>
 #include <string.h>
 
 int main(int argc, char **argv) {
     double tab[4] = {0.0, 1.0, 1.0, 2.0};
-    int cols[1] = {1};
-    void *table = ModelicaStandardTables_CombiTimeTable_init("NoName", "NoName", tab, 2, 2, 0.0, cols, 1, 1, 0);
+    int cols[1] = {2};
+    double tmin, tmax, te, y;
+    void *table = ModelicaStandardTables_CombiTimeTable_init2("NoName", "NoName", tab, 2, 2, 0.0, cols, 1, 1, 3, 0.0, 3, 1);
     assert(table);
+    tmin = ModelicaStandardTables_CombiTimeTable_minimumTime(table);
+    assert(fabs(tmin - 0.0) < 1e-6);
+    tmax = ModelicaStandardTables_CombiTimeTable_maximumTime(table);
+    assert(fabs(tmax - 1.0) < 1e-6);
+    te = ModelicaStandardTables_CombiTimeTable_nextTimeEvent(table, 1.5);
+    y = ModelicaStandardTables_CombiTimeTable_getValue(table, 1, 1.5, te, te);
+    assert(fabs(y - (1.0 + 2.0)/2) < 1e-6);
+    ModelicaStandardTables_CombiTimeTable_close(table);
     return 0;
 }
--- .CI/Test/test.sh
+++ .CI/Test/test.sh
@@ -12,6 +12,10 @@ if test ! "$1" = "nostatic"; then
 ./a.out || exit 1
 "$CC" -L $LIBRARIES Tables.c -Wl,-Bstatic -lModelicaStandardTables -lModelicaIO -lModelicaMatIO -lzlib -Wl,-Bdynamic -lm || exit 1
 ./a.out || exit 1
+"$CC" -L $LIBRARIES TablesFromTxtFile.c -Wl,-Bstatic -lModelicaStandardTables -lModelicaIO -lModelicaMatIO -lzlib -Wl,-Bdynamic -lm || exit 1
+./a.out || exit 1
+"$CC" -L $LIBRARIES TablesFromMatFile.c -Wl,-Bstatic -lModelicaStandardTables -lModelicaIO -lModelicaMatIO -lzlib -Wl,-Bdynamic -lm || exit 1
+./a.out || exit 1
 "$CC" -L $LIBRARIES TablesNoUsertab.c -Wl,-Bstatic -lModelicaStandardTables -lModelicaIO -lModelicaMatIO -lzlib -Wl,-Bdynamic -lm || exit 1
 ./a.out || exit 1
 
@@ -24,6 +28,10 @@ if test ! "$1" = "onlystatic"; then
 "$CC" -L $LIBRARIES -Wl,-rpath $LIBRARIES ModelicaStrings.c -lModelicaExternalC || exit 1
 ./a.out || exit 1
 "$CC" -L $LIBRARIES -Wl,-rpath $LIBRARIES Tables.c -lModelicaStandardTables -lModelicaIO -lModelicaMatIO || exit 1
+./a.out || exit 1
+"$CC" -L $LIBRARIES -Wl,-rpath $LIBRARIES TablesFromMatFile.c -lModelicaStandardTables -lModelicaIO -lModelicaMatIO || exit 1
+./a.out || exit 1
+"$CC" -L $LIBRARIES -Wl,-rpath $LIBRARIES TablesFromTxtFile.c -lModelicaStandardTables -lModelicaIO -lModelicaMatIO || exit 1
 ./a.out || exit 1
 "$CC" -L $LIBRARIES -Wl,-rpath $LIBRARIES TablesNoUsertab.c -lModelicaStandardTables -lModelicaIO -lModelicaMatIO || exit 1
 ./a.out || exit 1
